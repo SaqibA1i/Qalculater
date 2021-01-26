9fb4a59c36e68dd10f715223791c21ea8046bb42
saqib ali <saqibalip321@gmail.com>
2021-01-26 08:13:46 -0500

    fix: pushing build as havent set up react build scripts in heroku

---
+++ src/main/resources/static/precache-manifest.e54eb8977a1ecd7258c986a9da2dd465.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545056c7ee81bd746439cc87b20c07eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "816ef53d413655146c57",
+    "url": "/static/css/2.8497c2bb.chunk.css"
+  },
+  {
+    "revision": "bca649fd7b64b72f7743",
+    "url": "/static/css/main.35c1c652.chunk.css"
+  },
+  {
+    "revision": "816ef53d413655146c57",
+    "url": "/static/js/2.ca4dd782.chunk.js"
+  },
+  {
+    "revision": "641d1996b99f65a5dc87ce2f01c4f46e",
+    "url": "/static/js/2.ca4dd782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bca649fd7b64b72f7743",
+    "url": "/static/js/main.856b0d42.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
