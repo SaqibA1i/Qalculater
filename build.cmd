2b6ad0dd58499fb2c50f05873f79b1df5a2a68f4
saqib ali <saqibalip321@gmail.com>
2021-04-25 03:40:07 -0400

    getting express set up

--- build.cmd
+++
@@ -1,4 +0,0 @@
-DEL /F/Q/S "react-site/site/build/"
-DEL /F/Q/S "src/main/resources/static/" 
-cd react-site/site/
-npm run build && cd ../../ && xcopy /s "react-site/site/build" "src/main/resources/static" && git add . && git commit -m "Test" && git push
