976eae91b80b8663a271bb0264ac4a9962acc807
saqib ali <saqibalip321@gmail.com>
2021-04-30 04:03:38 -0400

    no env

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ require('dotenv').config();
  * DB_STRING=mongodb://<user>:<password>@localhost:27017/database_name
  */ 
 
-const conn = process.env.DB_STRING;
+const conn = "mongodb+srv://saq:YdjswdEfn5kN3UKl@cluster0.udjaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connection = mongoose.createConnection(conn, {
     useNewUrlParser: true,
