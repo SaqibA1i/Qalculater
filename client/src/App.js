5fb2ff7f79f02abe2a6b4662bbfcb828d0e356ae
saqib ali <saqibalip321@gmail.com>
2021-05-02 14:17:18 -0400

    await update

--- client/src/App.js
+++ client/src/App.js
@@ -262,7 +262,7 @@ function App() {
     endLoadingAnim();
     console.log("The user data was updated");
     alert(isAllDataUpdated)
-    alert(newData);
+    alert(JSON.stringify(newData));
   }
   return (
     <Router className="App" >
