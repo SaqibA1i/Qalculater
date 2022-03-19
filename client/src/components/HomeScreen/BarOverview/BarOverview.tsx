f045ce44d09117d0483689c2375f5faa5c961026
saqib ali <saqibalip321@gmail.com>
2022-03-19 17:34:39 -0400

    added bar responsivness

--- client/src/components/HomeScreen/BarOverview/BarOverview.tsx
+++ client/src/components/HomeScreen/BarOverview/BarOverview.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useQalcContext } from "../../../context/qalculaterContext";
 import {
   getCoursePercentageMapFromTerm,
@@ -10,10 +10,17 @@ import {
   coursePercentageSingle,
 } from "../../../TS types/Types";
 
-function BarOverview() {
+interface BarProps {
+  termAvg: number;
+}
+
+function BarOverview(Props: BarProps) {
   const [coursePercentageMap, setCoursePercentageMap] =
     useState<CoursePercentageMap>([]);
+  const [avg, setAvg] = useState<number>(389);
+
   const { userInfo, selection, setSelection } = useQalcContext()!;
+
   const getColor = (value: number) => {
     if (value < 0.5) {
       value = 1;
@@ -22,8 +29,9 @@ function BarOverview() {
     }
     //value from 0 to 1
     var hue = ((1 - value) * 120).toString(10);
-    return ["hsl(", hue, ",100%,50%)"].join("");
+    return ["hsl(", hue, ",80%,50%)"].join("");
   };
+
   useEffect(() => {
     let courses: CoursePercentageMap | void = getCoursePercentageMapFromTerm(
       selection.currTerm,
@@ -45,35 +53,63 @@ function BarOverview() {
     let courses = coursePercentageMap.length;
     return 70 / courses + "vw"; // 5 margin each side
   };
+
   return (
     <div className="bar-overview" id="bar-overview">
-      {coursePercentageMap.map((singleMap, idx) => {
-        return (
-          <div className="bar-container" key={idx}>
-            <div
-              className="bar"
-              onClick={() => {
-                selection.currCourse = singleMap[0];
-                setSelection({ ...selection });
-                //TODO add edit panel for courses;
-              }}
-              style={{
-                height: singleMap[1] * 2 + "px",
-                width: getWidth(),
-                background:
-                  "linear-gradient(" +
-                  getColor((singleMap[1] + 3) / 100) +
-                  ", " +
-                  getColor((singleMap[1] - 10) / 100) +
-                  ")",
-              }}
-            >
-              <p>{singleMap[1].toFixed(2)}%</p>
+      <div className="avgLine" style={{ top: -2 * Props.termAvg + 235 }}>
+        <p>{Props.termAvg}</p>
+        <hr />
+      </div>
+      <div className="container">
+        {coursePercentageMap.map((singleMap, idx) => {
+          let width = getWidth();
+          return (
+            <div className="bar-container" key={idx}>
+              <div
+                className="bar"
+                onClick={() => {
+                  selection.currCourse = singleMap[0];
+                  setSelection({ ...selection });
+                  //TODO add edit panel for courses;
+                }}
+                style={{
+                  height: singleMap[1] * 2 + "px",
+                  width: width + "px",
+                  background:
+                    "linear-gradient(" +
+                    getColor((singleMap[1] + 3) / 100) +
+                    ", " +
+                    getColor((singleMap[1] - 10) / 100) +
+                    ")",
+                  boxShadow:
+                    "0px 0px 9px 0px " + getColor((singleMap[1] - 10) / 100),
+                }}
+              >
+                <p>{singleMap[1].toFixed(2)}%</p>
+              </div>
+              <p
+                key={idx}
+                style={
+                  singleMap[0] === selection.currCourse
+                    ? {
+                        fontWeight: 900,
+                        maxWidth: width + "px",
+                      }
+                    : {
+                        maxWidth: width + "px",
+                        borderBottom: "1px solid transparent",
+                      }
+                }
+                className={
+                  singleMap[0] === selection.currCourse ? "selected" : ""
+                }
+              >
+                {singleMap[0]}
+              </p>
             </div>
-            <p key={idx}>{singleMap[0]}</p>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
